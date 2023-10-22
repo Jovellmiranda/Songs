b46b8fb265dde7e8538b93ae1d8d74e2d80db756
Jovellmiranda <jcqmiranda.student@ua.edu.ph>
2023-10-22 20:20:28 +0800

    third commit

--- Music.js
+++ Music.js
@@ -1,59 +1,57 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-// Jovell Christian Q. Miranda.
-//BSIT-3A
 var song = {
-   "song7" : {
+   "song8" : {
       "Title" : "Oksihina ",
-      "Lyrics by" : "Dionela",
+      "Artist" : "Dionela",
       "Release Date" : "September 8, 2023",
       "Genre":"Pop",
       "Link":"https://www.youtube.com/watch?v=zRCjgZIua_A",
-      "id": 7
+      "id": 8
     }
 }
-
+//Show the list
 app.get('/listSongs', function (req, res) {
-   fs.readFile( __dirname + "/" + "songs.json", 'utf8', function (err, data) {
+   fs.readFile( __dirname + "/" + "Songs.json", 'utf8', function (err, data) {
       console.log( data );
       res.end( data );
    });
 })
-
+//Add Song
 app.post('/addSong', function (req, res) {
     // First read existing songs.
-    fs.readFile( __dirname + "/" + "songs.json", 'utf8', function (err, data) {
+    fs.readFile( __dirname + "/" + "Songs.json", 'utf8', function (err, data) {
        data = JSON.parse( data );
-       data["song7"] = song["song7"];
+       data["song8"] = song["song8"];
        console.log( data );
        res.end( JSON.stringify(data));
     });
 })
-
+//Show specific song
 app.get('/:id', function (req, res) {
     // First read existing songs.
-    fs.readFile( __dirname + "/" + "songs.json", 'utf8', function (err, data) {
+    fs.readFile( __dirname + "/" + "Songs.json", 'utf8', function (err, data) {
        var songs = JSON.parse( data );
        var song = songs["song" + req.params.id];
        console.log( song );
        res.end( JSON.stringify(song));
     });
 })
-
+//Delete a Song
 app.delete('/deleteSong', function (req, res) {
    // First read existing songs.
-   fs.readFile( __dirname + "/" + "songs.json", 'utf8', function (err, data) {
+   fs.readFile( __dirname + "/" + "Songs.json", 'utf8', function (err, data) {
       data = JSON.parse( data );
-      delete data["song2"];
+      delete data["song5"];
       console.log( data );
       res.end( JSON.stringify(data));
    });
 })
-
+//delete specific song
 app.delete('/deleteSong/:id', function (req, res) {
     // First read existing songs.
-    fs.readFile( __dirname + "/" + "songs.json", 'utf8', function (err, data) {
+    fs.readFile( __dirname + "/" + "Songs.json", 'utf8', function (err, data) {
        data = JSON.parse( data );
        delete data["song" + req.params.id];
        console.log( data );
